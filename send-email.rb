4b6a06a471b69309b286e770f465a758b7806155
AndyRama <andyramaroson@gmail.com>
2021-08-23 23:49:17 +0200

    add migration for MailerContact

--- send-email.rb
+++ send-email.rb
@@ -1,18 +1,18 @@
 require 'sendgrid-ruby'
 include SendGrid
+require 'json'
 
-def hello_world
-  from = SendGrid::Email.new(email: 'andyramaroson@gmail.com')
-  to = SendGrid::Email.new(email: 'andyramaroson@gmail.com')
-  subject = ' sending with twilio SendGrid is fun '
-  content = SendGrid::Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby !')
-  mail = SendGrid::Mail.new(from, subject, to, content)
+from = SendGrid::Email.new(email: 'Andy-rama@hotmail.fr')
+to = SendGrid::Email.new(email: 'sender@gmail.com')
+subject = ' sending with twilio SendGrid is fun '
+content = SendGrid::Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby !')
+mail = SendGrid::Mail.new(from, subject, to, content)
 
-  sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-  response = sg.client.mail._('send').post(request_body: mail.to_json)
+puts mail.to_json
 
-  puts response.status_code
-  puts response.body
-  puts response.parsed_body
-  puts response.headers
-end
+sg = SendGrid::API.new(api_key: 'SENDGRID_API_KEY')
+response = sg.client.mail._('send').post(request_body: mail.to_json)
+
+puts response.status_code
+puts response.body
+puts response.headers
